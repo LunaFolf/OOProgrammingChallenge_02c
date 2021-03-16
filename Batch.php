5032ad4f7ec1bb4722f90d93b5952ed310c079b8
Liam Holmes <email@liamholmes.co.uk>
2021-03-16 18:58:23 +0000

    Batch and Main Classes commented

--- Batch.php
+++ Batch.php
@@ -3,18 +3,35 @@
 
   use App\Consignment;
 
-class Batch
+  /**
+   * Batch Class, for storing consignments.
+   */
+  class Batch
   {
+    /** @var array|null $consignments contains an array of stored consignments. */
     private $consignments;
 
+    /**
+     * Initialises the empty array.
+     * @return void
+     */
     public function __construct() {
       $this->consignments = [];
     }
 
+    /**
+     * Store consignment in the array.
+     * @param Consignment $consignment
+     * @return void
+     */
     public function addConsignment(Consignment $consignment) {
       array_push($this->consignments, $consignment);
     }
 
+    /**
+     * Get currently stored consignments
+     * @return array
+     */
     public function getConsignments() {
       return $this->consignments;
     }
--- Main.php
+++ Main.php
@@ -10,22 +10,43 @@
   use App\Batch;
   use App\Consignment;
 
-class Main
+  /**
+   * Main Class, Handles batch control.
+   */
+  class Main
   {
+    /** @var Batch $consignmentBatch contains an array of stored consignments. */
     public $consignmentBatch;
 
+    /**
+     * Initialises the empty Batch array, by calling the resetBatch function.
+     * @return void
+     */
     public function __construct() {
       $this->resetBatch();
     }
 
+    /**
+     * Initialises the empty Batch array.
+     * @return void
+     */
     public function resetBatch() {
       $this->consignmentBatch = new Batch;
     }
 
+    /**
+     * Add consignment to the current batch.
+     * @param Consignment $consignment
+     * @return void
+     */
     public function addConsignment(Consignment $consignment) {
       $this->consignmentBatch->addConsignment($consignment);
     }
 
+    /**
+     * End current batch and begin shipping/sending the consignments.
+     * @return void
+     */
     public function endBatch() {
       $consignments = $this->consignmentBatch->getConsignments();
       foreach ($consignments as $consignment) {
