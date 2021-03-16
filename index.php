5b43f80e9b77b85b915a6d5279b1c4c675b92a2c
Liam Holmes <email@liamholmes.co.uk>
2021-03-16 19:01:35 +0000

    Added some commenting on my index file for good measure

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+  // This is my index file for testing functionality, so apologies for jank/dirty code.
+
   namespace App;
   require_once('Main.php');
 
+  // Create the Main Class
   $main = new Main();
 
+  // Initialise the array for test values.
   $consignmentsArray = [
     [ "Keyboard", "RM" ],
     [ "Mouse", "RM" ],
@@ -12,6 +16,12 @@
     [ "Phone", "RM" ]
   ];
 
+  /**
+   * for each test value in the array,
+   * Create a new Consignment class,
+   * generate a unqiue courier consignment number,
+   * and add the consignment to the current batch. 
+   */
   foreach ($consignmentsArray as $consignment) {
     $consignment = new Consignment($consignment[0], $consignment[1]);
     $consignment->generateNumberFromCourier();
@@ -22,5 +32,6 @@
 
   print("<hr>");
 
+  // End the current batch, expected output is a list of the jobs that will be queued for shipping/sending.
   $main->endBatch();
 ?>
