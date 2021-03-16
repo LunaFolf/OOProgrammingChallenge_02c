21f6f73fcc8d40e87e593e678a2ff68ab13d0553
Liam Holmes <email@liamholmes.co.uk>
2021-03-16 18:50:52 +0000

    Commented Courier Class

--- Couriers/Courier.php
+++ Couriers/Courier.php
@@ -5,32 +5,60 @@
 
 class Courier
   {
+    /** @var string|null $courierName contains a user-friendly representation of the Courier's name. */
     protected $courierName = null;
+    /** @var string|null $transportMethod contains an identifier for the method the consignment's data will be transported. */
     protected $transportMethod = null;
 
+
+    /**
+     * Get user-friendly Courier name.
+     * @return string|null
+     */
     public function getCourierName() {
       return $this->courierName;
     }
 
-    public function setCourierName($courierName) {
-      $this->courierName = $courierName;
-    }
-
+    /**
+     * Get transport method identifier.
+     * @return string|null
+     */
     public function getTransportMethod() {
       return $this->transportMethod;
     }
 
+    /**
+     * Set user-friendly Courier name.
+     * @param string|null $courierName
+     * @return void
+     */
+    public function setCourierName($courierName) {
+      $this->courierName = $courierName;
+    }
+
+    /**
+     * Set transport method identifier.
+     * @param string|null $transportMethod
+     * @return void
+     */
     public function setTransportMethod($transportMethod) {
       $this->transportMethod = $transportMethod;
     }
 
     /**
-     * This function should be overriden by extending classes
+     * A unique algorithm for generating courier consignment numbers.
+     * This should be overidden in extending clases.
+     * @return string|int|null
      */
     public function generateConsignmentNumber() {
       return "TBC";
     }
 
+    /**
+     * Send the consignment via the saved $this->transportMethod
+     * @param Consignment $consignment
+     * @return void
+     */
     public function sendConsignment(Consignment $consignment) {
       if (!$this->transportMethod) {
         throw new \Exception("No Data Transport Method has been set for $this->courierName.");
